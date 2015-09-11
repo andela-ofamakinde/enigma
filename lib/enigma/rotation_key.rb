642b5b4f0180953f4e3bde5a16260b8072f9cb6a
andela-ofamamkinde <olatoyosi.famakinde@andela.co>
2015-09-11 14:10:38 +0100

    Refactor Cipher method

--- lib/enigma/rotation_key.rb
+++ lib/enigma/rotation_key.rb
@@ -1,16 +1,16 @@
 module Enigma
   class Rotation 
     def construct_rotation_hash(rotation, date, text)
-     @rotation = rotation.split("")
-     @new_date = (date.to_i**2).to_s.split("").last(4)
-     @cypher_array = []
+     rotation_array = rotation.split("")
+     new_date = (date.to_i**2).to_s.split("").last(4)
+     cypher_array = []
      i = 0
      while i < 4
-       @cypher_array << ("#{@rotation[i]}" + "#{@rotation[i+1]}").to_i + (@new_date[i]).to_i
+       cypher_array << ("#{rotation_array[i]}" + "#{rotation_array[i+1]}").to_i + (new_date[i]).to_i
        i+=1
      end
-     @cypher_array 
-     rotation_hash = Hash[@cypher_array.zip(text)]
+     cypher_array 
+     rotation_hash = Hash[cypher_array.zip(text)]
      rotation_hash
    end
   end
